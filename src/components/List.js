dd81660787122dc38bc007d924904745eac6f915
Mungkorn Chulaprungrueang <mungkonmoto@gmail.com>
2018-07-01 10:12:57 +0700

    commit message

--- src/components/List.js
+++ src/components/List.js
@@ -2,23 +2,23 @@ import React, { Component } from "react";
 import ListItems from "./ListItems";
 
 class List extends Component {
- constructor(props) {
-   super(props);
-   this.state = {};
- }
- render() {
-   return (
-<div
-       style={{
-         padding: 2,
-         height: 400,
-         overflow: "scroll",
-         overflowX: "hidden"
-       }}
-     >
-       <ListItems todo={this.props.todo}/>
-     </div>
-   );
- }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div
+        style={{
+          padding: 2,
+          height: 400,
+          overflow: "scroll",
+          overflowX: "hidden"
+        }}
+      >
+        <ListItems todo={this.props.todo} />
+      </div>
+    );
+  }
 }
 export default List;
